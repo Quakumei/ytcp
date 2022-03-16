2bef9ddf40d89d0cae3fc708f4441a3d7d684274
Quakumei <quakumei@gmail.com>
2022-03-17 02:31:28 +0300

    feat(args): args sanity checks

--- ytcp.py
+++ ytcp.py
@@ -3,11 +3,23 @@
     YTCP - python3 script to create playlist vids
 """
 
+# youtube_dl
 import youtube_dl
+
+# args check
 import argparse
+import re
 from os.path import exists
 
-def parse_args():
+#utility (stderr)
+import sys
+
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+
+def parse_args() -> argparse.Namespace:
     """
         Parses arguments, yes
     :return: returns parsed arguments by argparse
@@ -29,7 +41,34 @@ def parse_args():
     return parser.parse_args()
 
 
+def check_args(args: argparse.Namespace):
+    """
+        Runs sanity checks for arguments and ends program exec if something is wrong.
+    """
+    if (args.foreground_image and not exists(args.foreground_image)):
+        raise FileNotFoundError("foreground_image")
+
+    if (args.background_image and not exists(args.background_image)):
+        raise FileNotFoundError("background_image")
+
+    if (args.list and not exists(args.list)):
+        raise FileNotFoundError("list")
+
+    if (not re.fullmatch('[0-9]{2,5}x[0-9]{2,5}', args.resolution)):
+        raise ValueError("resolution")
+
 
 if __name__ == "__main__":
     args = parse_args()
-    check_args(args)
+    try:
+        check_args(args)
+    except Exception as e:
+        eprint("[ERR] : " + str(e.__class__) + " : " + str(e))
+
+    resolution = args.resolution.split('x')
+
+    resolution_w = resolution[0]
+    resolution_h = resolution[1]
+    bg_img = args.background_image
+    fg_img = args.foreground_image
+    list_file = args.list
